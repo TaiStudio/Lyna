fa1e70eefa4dfea21df16848ccfdd24d67bbe8ff
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-06-29 19:05:57 +0200

    🖋 Update

--- builder.js
+++ builder.js
@@ -61,6 +61,7 @@ function getFiles (dir, files_){
         `);
 
         fs.writeFileSync(`./dist/sitemap.xml`, sitemap.html());
+        fs.writeFileSync(`sitemap.xml`, sitemap.html());
     }
     return files_;
 }
