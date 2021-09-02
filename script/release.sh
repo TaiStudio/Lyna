87a29efd39247a00fdbb427f92cf5cc2c2bcd5a0
Tai Studio <tai.studio@outlook.fr>
2021-09-02 19:33:55 +0200

    🖋 Update

--- script/release.sh
+++ script/release.sh
@@ -5,8 +5,8 @@ set -o errexit    # always exit on error
 set -o pipefail   # honor exit codes when piping
 set -o nounset    # fail on unset variables
 
-git clone "https://AnimeBack-Bot:$GH_TOKEN@github.com/TaiStudio/Lyna" setuper
-cd setuper
+git clone "https://AnimeBack-Bot:$GH_TOKEN@github.com/TaiStudio/Lyna" lyna
+cd lyna
 npm install
 
 npm run setup
