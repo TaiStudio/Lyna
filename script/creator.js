f9c905254790736df27d2d8fa20068850a38c147
Tai Studio <tai.studio@outlook.fr>
2022-04-27 20:12:08 +0200

    🖋 Update

--- script/creator.js
+++ script/creator.js
@@ -47,7 +47,7 @@ function colors(){
         if(config[l].startsWith('>') == true){
             let temp = config[l].split(',');
 
-            var name = temp[0].replace('>', ''),
+            var name = temp[0].replace('\>', ''),
                 link = replaceAll(replaceAll(temp[1], '(', ''), ')', ''),
                 service = getService(link);
 
