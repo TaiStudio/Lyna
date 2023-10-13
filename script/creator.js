7a34a199027bf7b630b1faa8734d91844641261a
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-10-13 14:56:08 +0200

    FIX: vuln (part 2)

--- script/creator.js
+++ script/creator.js
@@ -8,16 +8,32 @@
 \-----------------------------------------------------------------------------------------------------------*/
 
 const fs = require('fs');
-const request = require('request');
+const axios = require('axios');
 const listService = require('../lib/services.json');
 
-var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
+const download = (uri, filename, callback) => {
+    axios.head(uri)
+        .then(response => {
+            console.log('content-type:', response.headers['content-type']);
+            console.log('content-length:', response.headers['content-length']);
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
+            axios({
+                method: 'get',
+                url: uri,
+                responseType: 'stream', // This ensures the response is treated as a stream
+            })
+                .then(response => {
+                    response.data.pipe(fs.createWriteStream(filename)).on('close', callback);
+                })
+                .catch(error => {
+                    console.error('Error downloading the file:', error);
+                    callback(error);
+                });
+        })
+        .catch(error => {
+            console.error('Error getting headers:', error);
+            callback(error);
+        });
 };
 
 var config = process.argv[2].replace('--body=', '');
@@ -42,10 +58,10 @@ var prod = {
 
 console.log(config);
 
-function init(){
+function init() {
     console.log(mode);
-    if(mode == "edit"){
-        if(config[0].startsWith('###') == true){
+    if (mode == "edit") {
+        if (config[0].startsWith('###') == true) {
             let name = replaceAll(config[0], '### ', '');
             name = name.toLowerCase();
             prod = JSON.parse(fs.readFileSync(`pages/${name}/${name}.json`).toString());
@@ -58,80 +74,80 @@ function init(){
 }
 init();
 
-function filter(){
-    for(f=0;f<config.length;f++){
-        if(config[f].startsWith('###') == true){
+function filter() {
+    for (f = 0; f < config.length; f++) {
+        if (config[f].startsWith('###') == true) {
             setName(config[f]);
             createDir(prod.name.toLowerCase());
         }
-        if(config[f].startsWith('## ') == true){
+        if (config[f].startsWith('## ') == true) {
             setDescription(config[f]);
         }
-        if(config[f].startsWith('>') == true){
+        if (config[f].startsWith('>') == true) {
             colors(config[f]);
         }
-        if(config[f].startsWith('[') == true){
+        if (config[f].startsWith('[') == true) {
             links(config[f]);
         }
-        if(config[f].startsWith('Logo:') == true){
+        if (config[f].startsWith('Logo:') == true) {
             logo(config[f]);
         }
-        if(config[f].startsWith('Background:') == true){
+        if (config[f].startsWith('Background:') == true) {
             background(config[f]);
         }
     }
 }
 
-function logo(arg){
-    if(arg.startsWith('Logo:') == true){
+function logo(arg) {
+    if (arg.startsWith('Logo:') == true) {
         let temp = arg.split(']');
 
         var link = replaceAll(replaceAll(temp[1], '(', ''), ')', '');
-        
-        download(link, `pages/${prod.name.toLowerCase()}/logo.png`, function(){
+
+        download(link, `pages/${prod.name.toLowerCase()}/logo.png`, function () {
             console.log('done');
         });
         setProd('logo', `pages/${prod.name.toLowerCase()}/logo.png`);
     }
 }
-function background(arg){
-    if(arg.startsWith('Background:') == true){
+function background(arg) {
+    if (arg.startsWith('Background:') == true) {
         let temp = arg.split(']');
 
         var link = replaceAll(replaceAll(temp[1], '(', ''), ')', '');
-        
-        var url = link.substring(link.lastIndexOf('/')+1);
 
-        download(link, `pages/${prod.name.toLowerCase()}/${url}`, function(){
+        var url = link.substring(link.lastIndexOf('/') + 1);
+
+        download(link, `pages/${prod.name.toLowerCase()}/${url}`, function () {
             console.log('done');
         });
         setProd('background', `pages/${prod.name.toLowerCase()}/${url}`);
     }
 }
-function colors(arg){
-    if(arg.startsWith('>') == true){
+function colors(arg) {
+    if (arg.startsWith('>') == true) {
         let temp = arg.split(',');
         temp[0] = temp[0].replace('> ', '');
-        for(l2=0;l2<temp.length;l2++){
+        for (l2 = 0; l2 < temp.length; l2++) {
             temp[l2] = replaceAll(temp[l2], ' ', '');
         }
         setProd('colors', temp);
     }
 }
-function getService(link){
+function getService(link) {
     var url = new URL(link);
     var srv = url.host.split('.')[0];
 
-    if(listService.all.includes(`${srv}.png`) == true){
+    if (listService.all.includes(`${srv}.png`) == true) {
         srv = srv;
     }
-    else{
+    else {
         srv = 'Internet';
     }
     return srv;
 }
-function links(arg){
-    if(arg.startsWith('[') == true){
+function links(arg) {
+    if (arg.startsWith('[') == true) {
         let temp = arg.split(']');
 
         var name = temp[0].replace('\[', ''),
@@ -141,7 +157,7 @@ function links(arg){
         addLink(name, link, service);
     }
 }
-function addLink(name, link, service){
+function addLink(name, link, service) {
     var temp = {
         "name": name,
         "service": service,
@@ -152,8 +168,8 @@ function addLink(name, link, service){
     setProd('links', temp);
 }
 
-function setProd(key, value){
-    switch(key){
+function setProd(key, value) {
+    switch (key) {
         case 'name':
             prod[key] = value.charAt(0).toUpperCase() + value.slice(1);
             break;
@@ -166,37 +182,37 @@ function setProd(key, value){
     console.log(prod);
 }
 
-function setName(arg){
-    if(arg.startsWith('###')){
+function setName(arg) {
+    if (arg.startsWith('###')) {
         setProd('name', replaceAll(arg, '### ', ''));
     }
 }
 
-function setDescription(arg){
-    if(arg.startsWith('##')){
+function setDescription(arg) {
+    if (arg.startsWith('##')) {
         setProd('description', replaceAll(arg, '## ', ''));
     }
 }
 
-function createDir(dir){
-    if(fs.existsSync(`pages/${dir}`) == false){
+function createDir(dir) {
+    if (fs.existsSync(`pages/${dir}`) == false) {
         fs.mkdirSync(`pages/${dir}`);
     }
-    else{
+    else {
         console.log('ERROR PAGE ALREADY EXIST !');
     }
 }
 
-function savePage(dir){
-    if(fs.existsSync(`pages/${dir}`) == true){
+function savePage(dir) {
+    if (fs.existsSync(`pages/${dir}`) == true) {
         fs.writeFileSync(`pages/${dir}/${dir}.json`, JSON.stringify(prod, null, 2));
     }
-    else{
+    else {
         console.log('ERROR SAVE PAGE !');
     }
 }
 
 function replaceAll(str, find, replace) {
-	var escapedFind=find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-	return str.replace(new RegExp(escapedFind, 'g'), replace);
+    var escapedFind = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    return str.replace(new RegExp(escapedFind, 'g'), replace);
 }
